27df342fe787b0ff4391c6686fe4ad2546685b69
POmaster <horrorandscifi@gmail.com>
2026-02-07 00:59:46 +0300

    Create veryeasy.bat

---
+++ veryeasy.bat
@@ -0,0 +1,6 @@
+!!!ATTENTION!!! First, install Git and then run git init
+git add from the command line.
+git commit -m "Initial commit: creating the mail interface"
+git branch -M main
+git remote add origin https://github.com
+git push -u origin main
